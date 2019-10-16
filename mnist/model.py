7caaa3b91490913b7264a3ba859a6265d5a83d47
Henri <t.henri.al@gmail.com>
2019-10-16 16:02:47 +0200

    model class and tests

--- mnist/model.py
+++ mnist/model.py
@@ -1,2 +1,24 @@
+import pathlib
+import joblib
+from PIL import Image
+
+
 class Model:
-    pass
+
+    def __init__(self):
+        self.clf = None
+    
+    def load_model(self, model_path: pathlib.Path):
+        self.clf = joblib.load(model_path)
+
+    def predict(self, img_vect: list):
+        if not isinstance(img_vect, list):
+            raise TypeError
+        result = self.clf.predict([img_vect])
+        return result
+
+    @classmethod
+    def from_model(self, model_path: pathlib.Path):
+        model = Model()
+        model.load_model(model_path)
+        return model
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,6 +1,51 @@
+import pytest
+import joblib
+import random
 from mnist.model import Model
 
 
-def test_class_exitsts():
+@pytest.fixture
+def model_path():
+    return "models/mnist_digits_clf.pkl"
+
+
+@pytest.fixture
+def data():
+    X_test, y_test = joblib.load("data/X_test_and_y_test.pkl")
+    return X_test, y_test
+
+
+@pytest.fixture
+def random_test_data():
+    return [random.randint(0, 255) for i in range(784)]
+
+
+@pytest.fixture
+def prediction_range():
+    return range(0, 10)
+
+
+def test_class_exists():
     model = Model()
     assert model
+
+
+def test_load_model(model_path):
+    model = Model()
+    model.load_model(model_path)
+    assert model.clf
+
+
+def test_from_model(model_path):
+    model = Model.from_model(model_path)
+    assert model and model.clf
+
+
+def test_predict(model_path, random_test_data):
+    model = Model.from_model(model_path)
+
+    prediction = int(model.predict(random_test_data))
+
+    assert prediction >= 0 and prediction <= 9 
+
+
